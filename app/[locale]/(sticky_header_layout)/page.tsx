ec579f083fbf08b6dca437c10bc5f7f3d8a1ac71
Sue Lee <soojlee0106@naver.com>
2025-05-29 08:17:46 +0900

    refactor: 구조 수정

--- app/[locale]/(sticky_header_layout)/page.tsx
+++ app/[locale]/(sticky_header_layout)/page.tsx
@@ -1,6 +1,3 @@
-import { useTranslations } from "next-intl";
-
 export default function Home() {
-  const tr = useTranslations('HomePage');
-  return <h1>{tr('title')}</h1>;
+  return <div>Home- Page with header layout</div>;
 }
